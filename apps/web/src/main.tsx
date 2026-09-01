50574e912b865ae1a4ca24a4b9869ddeb6341db1
Jay Wedgeworth <12656028+jaywedgeworth22@users.noreply.github.com>
2026-09-01 06:30:54 -0500

    Add lightweight web Sentry client observability to Autorotate web

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter } from 'react-router'
 import './index.css'
 import { TRPCProvider } from "@/providers/trpc"
 import App from './App.tsx'
+import { initSentry } from './lib/observability/sentry'
+
+initSentry();
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
